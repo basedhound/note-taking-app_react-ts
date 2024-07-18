e0725dc01cc8a96bc8f526c39a418817f76e55f9
Jozef S <franck.vukelic@gmail.com>
2024-07-18 18:42:42 +0700

    Update

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react"
+import { useMemo, useState } from "react";
 import {
   Badge,
   Button,
@@ -8,32 +8,32 @@ import {
   Modal,
   Row,
   Stack,
-} from "react-bootstrap"
-import { Link } from "react-router-dom"
-import ReactSelect from "react-select"
-import { Tag } from "./App"
-import styles from "./NoteList.module.css"
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { Tag } from "./App";
+import styles from "./NoteList.module.css";
 
 type SimplifiedNote = {
-  tags: Tag[]
-  title: string
-  id: string
-}
+  tags: Tag[];
+  title: string;
+  id: string;
+};
 
 type NoteListProps = {
-  availableTags: Tag[]
-  notes: SimplifiedNote[]
-  onDeleteTag: (id: string) => void
-  onUpdateTag: (id: string, label: string) => void
-}
+  availableTags: Tag[];
+  notes: SimplifiedNote[];
+  onDeleteTag: (id: string) => void;
+  onUpdateTag: (id: string, label: string) => void;
+};
 
 type EditTagsModalProps = {
-  show: boolean
-  availableTags: Tag[]
-  handleClose: () => void
-  onDeleteTag: (id: string) => void
-  onUpdateTag: (id: string, label: string) => void
-}
+  show: boolean;
+  availableTags: Tag[];
+  handleClose: () => void;
+  onDeleteTag: (id: string) => void;
+  onUpdateTag: (id: string, label: string) => void;
+};
 
 export function NoteList({
   availableTags,
@@ -41,38 +41,37 @@ export function NoteList({
   onUpdateTag,
   onDeleteTag,
 }: NoteListProps) {
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
-  const [title, setTitle] = useState("")
-  const [editTagsModalIsOpen, setEditTagsModalIsOpen] = useState(false)
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
+  const [editTagsModalIsOpen, setEditTagsModalIsOpen] = useState(false);
 
   const filteredNotes = useMemo(() => {
-    return notes.filter(note => {
+    return notes.filter((note) => {
       return (
         (title === "" ||
           note.title.toLowerCase().includes(title.toLowerCase())) &&
         (selectedTags.length === 0 ||
-          selectedTags.every(tag =>
-            note.tags.some(noteTag => noteTag.id === tag.id)
+          selectedTags.every((tag) =>
+            note.tags.some((noteTag) => noteTag.id === tag.id)
           ))
-      )
-    })
-  }, [title, selectedTags, notes])
+      );
+    });
+  }, [title, selectedTags, notes]);
 
   return (
     <>
       <Row className="align-items-center mb-4">
-        <Col>
-          <h1>Notes</h1>
-        </Col>
         <Col xs="auto">
-          <Stack gap={2} direction="horizontal">
+          <Stack
+            gap={2}
+            direction="horizontal"
+            className="d-flex justify-content-center align-items-center">
             <Link to="/new">
               <Button variant="primary">Create</Button>
             </Link>
             <Button
               onClick={() => setEditTagsModalIsOpen(true)}
-              variant="outline-secondary"
-            >
+              variant="outline-secondary">
               Edit Tags
             </Button>
           </Stack>
@@ -82,11 +81,11 @@ export function NoteList({
         <Row className="mb-4">
           <Col>
             <Form.Group controlId="title">
-              <Form.Label>Title</Form.Label>
+              <Form.Label>Search</Form.Label>
               <Form.Control
                 type="text"
                 value={title}
-                onChange={e => setTitle(e.target.value)}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </Form.Group>
           </Col>
@@ -94,18 +93,18 @@ export function NoteList({
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
               <ReactSelect
-                value={selectedTags.map(tag => {
-                  return { label: tag.label, value: tag.id }
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
                 })}
-                options={availableTags.map(tag => {
-                  return { label: tag.label, value: tag.id }
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
                 })}
-                onChange={tags => {
+                onChange={(tags) => {
                   setSelectedTags(
-                    tags.map(tag => {
-                      return { label: tag.label, id: tag.value }
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
                     })
-                  )
+                  );
                 }}
                 isMulti
               />
@@ -114,7 +113,7 @@ export function NoteList({
         </Row>
       </Form>
       <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
-        {filteredNotes.map(note => (
+        {filteredNotes.map((note) => (
           <Col key={note.id}>
             <NoteCard id={note.id} title={note.title} tags={note.tags} />
           </Col>
@@ -128,7 +127,7 @@ export function NoteList({
         availableTags={availableTags}
       />
     </>
-  )
+  );
 }
 
 function NoteCard({ id, title, tags }: SimplifiedNote) {
@@ -136,21 +135,18 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
     <Card
       as={Link}
       to={`/${id}`}
-      className={`h-100 text-reset text-decoration-none ${styles.card}`}
-    >
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}>
       <Card.Body>
         <Stack
           gap={2}
-          className="align-items-center justify-content-center h-100"
-        >
+          className="align-items-center justify-content-center h-100">
           <span className="fs-5">{title}</span>
           {tags.length > 0 && (
             <Stack
               gap={1}
               direction="horizontal"
-              className="justify-content-center flex-wrap"
-            >
-              {tags.map(tag => (
+              className="justify-content-center flex-wrap">
+              {tags.map((tag) => (
                 <Badge className="text-truncate" key={tag.id}>
                   {tag.label}
                 </Badge>
@@ -160,7 +156,7 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
         </Stack>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
 function EditTagsModal({
@@ -178,20 +174,19 @@ function EditTagsModal({
       <Modal.Body>
         <Form>
           <Stack gap={2}>
-            {availableTags.map(tag => (
+            {availableTags.map((tag) => (
               <Row key={tag.id}>
                 <Col>
                   <Form.Control
                     type="text"
                     value={tag.label}
-                    onChange={e => onUpdateTag(tag.id, e.target.value)}
+                    onChange={(e) => onUpdateTag(tag.id, e.target.value)}
                   />
                 </Col>
                 <Col xs="auto">
                   <Button
                     onClick={() => onDeleteTag(tag.id)}
-                    variant="outline-danger"
-                  >
+                    variant="outline-danger">
                     &times;
                   </Button>
                 </Col>
@@ -201,5 +196,5 @@ function EditTagsModal({
         </Form>
       </Modal.Body>
     </Modal>
-  )
+  );
 }
